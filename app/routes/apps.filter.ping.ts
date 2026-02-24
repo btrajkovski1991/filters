c73df22539f00cf3e857417d9e00d6bacc9cdcd4
btrajkovski1991 <b.trajkovski1991@gmail.com>
2026-02-24 22:40:49 +0100

    Delete app/routes/apps.filter.ping.ts

--- app/routes/apps.filter.ping.ts
+++
@@ -1,9 +0,0 @@
-export async function loader() {
-  return new Response("OK", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-      "Cache-Control": "no-store",
-    },
-  });
-}
